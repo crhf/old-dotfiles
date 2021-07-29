f59f06b10690ae09b0efb6e1aa843dd61f56c04f
crhf <ruanhf8@gmail.com>
2021-07-29 21:39:42 +0800

    prezto: zsh configuration

---
+++ prezto/setup.sh
@@ -0,0 +1,12 @@
+# `cd` into the prezto root directory and call `zsh setup.sh`
+
+setopt EXTENDED_GLOB
+# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  # ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+# done
+
+for rcfile in ./runcoms/^README.md(.N); do
+	ln -sf "$(pwd)/$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+ln -sf "$(pwd)" "${ZDOTDIR:-$HOME}/.zprezto"
