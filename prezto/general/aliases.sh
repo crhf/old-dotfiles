e0cb495319a813ef598be26dd8a058a994b3b851
crhf <ruanhf8@gmail.com>
2021-08-22 10:25:27 +0800

    ssh ServerAliveInterval

--- prezto/general/aliases.sh
+++ prezto/general/aliases.sh
@@ -7,3 +7,6 @@ alias zz='z -c'      # restrict matches to subdirs of $PWD
 alias zi='z -i'      # cd with interactive selection
 alias zf='z -I'      # use fzf to select in multiple matches
 alias zb='z -b'      # quickly cd to the parent directory
+
+# ssh
+alias ssh='ssh -o ServerAliveInterval=60'
