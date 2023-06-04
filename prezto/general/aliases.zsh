a409c7cd541ad1ac6a286a38535b4bafb98fab46
crhf <ruanhf8@gmail.com>
2023-06-04 12:19:30 +0800

    update aliases

--- prezto/general/aliases.zsh
+++ prezto/general/aliases.zsh
@@ -3,3 +3,21 @@ alias gs='git status'
 alias gb='git branch'
 alias gc='git commit -m'
 alias ga='git add'
+alias gw='git show'
+alias gd='git diff'
+
+alias mkvv='mkvirtualenv'
+alias lsvv='lsvirtualenv'
+alias showvv='showvirtualenv'
+alias rmvv='rmvirtualenv'
+alias cpvv='cpvirtualenv'
+alias allvv='allvirtualenv'
+alias wo='workon'
+alias da='deactivate'
+
+alias vim='nvim'
+alias v='nvim'
+
+alias s='source'
+
+alias ls='ls -F --color=auto'
