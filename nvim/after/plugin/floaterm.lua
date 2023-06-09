cc4a3fe1a1c8dfe1688d7fc4b6c5fb6de62fccd7
crhf <ruanhf8@gmail.com>
2023-06-09 10:56:10 +0800

    change floaterm keymap

--- nvim/after/plugin/floaterm.lua
+++ nvim/after/plugin/floaterm.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<F12>", ":FloatermToggle<CR>")
-vim.keymap.set("t", "<F12>", "<C-\\><C-n>:FloatermToggle<CR>")
+vim.keymap.set("n", "<C-n>", ":FloatermToggle<CR>")
+vim.keymap.set("t", "<C-n>", "<C-\\><C-n>:FloatermToggle<CR>")
 vim.keymap.set("n", "<leader>lg", ":FloatermNew lazygit<CR>")
