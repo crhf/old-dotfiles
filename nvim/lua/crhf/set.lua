db36f8b1eab2636b74e67afe9b3287536e6e72fa
crhf <ruanhf8@gmail.com>
2023-06-13 11:27:25 +0800

    feat: remove unused mappings

--- nvim/lua/crhf/set.lua
+++ nvim/lua/crhf/set.lua
@@ -57,13 +57,13 @@ vim.diagnostic.config({
 -- )
 -- Go to next diagnostic (if there are multiple on the same line, only shows
 -- one at a time in the floating window)
-vim.api.nvim_set_keymap(
-  'n', '<Leader>dn', ':lua vim.diagnostic.goto_next()<CR>',
-  { noremap = true, silent = true }
-)
+-- vim.api.nvim_set_keymap(
+--   'n', '<Leader>dn', ':lua vim.diagnostic.goto_next()<CR>',
+--   { noremap = true, silent = true }
+-- )
 -- Go to prev diagnostic (if there are multiple on the same line, only shows
 -- one at a time in the floating window)
-vim.api.nvim_set_keymap(
-  'n', '<Leader>dp', ':lua vim.diagnostic.goto_prev()<CR>',
-  { noremap = true, silent = true }
-)
+-- vim.api.nvim_set_keymap(
+--   'n', '<Leader>dp', ':lua vim.diagnostic.goto_prev()<CR>',
+--   { noremap = true, silent = true }
+-- )
