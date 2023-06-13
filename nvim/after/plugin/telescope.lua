82eb776365e7b606f85a4bae05e036961e494f9a
crhf <ruanhf8@gmail.com>
2023-06-13 12:03:43 +0800

    feat: change telescope mappings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,32 @@
+require("telescope").setup {
+    pickers = {
+        find_files = {
+            mappings = {
+                i = {
+                    ["<C-l>"] = "close",
+                    ["<Esc>"] = false,
+                    ["<C-c>"] = false
+                },
+            },
+        },
+        grep_string = {
+            mappings = {
+                i = {
+                    ["<C-l>"] = "close",
+                    ["<Esc>"] = false,
+                    ["<C-c>"] = false
+                }
+            }
+        }
+    },
+}
 local builtin = require('telescope.builtin')
 -- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pf', function ()
-    builtin.find_files({no_ignore=true})
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({ no_ignore = true })
 end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    -- builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string();
 end)
