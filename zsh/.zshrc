84f301b423a1d76e4dbaf8baa506ac1a809f37a0
crhf <ruanhf8@gmail.com>
2023-06-16 20:03:54 +0800

    feat: add zsh/ and starship.toml

---
+++ zsh/.zshrc
@@ -0,0 +1,25 @@
+# Created by Zap installer
+[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+# plug "jeffreytse/zsh-vi-mode"
+plug "zsh-users/zsh-autosuggestions"
+plug "zap-zsh/supercharge"
+plug "zap-zsh/zap-prompt"
+plug "zsh-users/zsh-syntax-highlighting"
+
+# Load and initialise completion system
+autoload -Uz compinit
+compinit
+
+plug $ZDOTDIR/exports.zsh
+plug $ZDOTDIR/aliases.zsh
+
+bindkey -v
+bindkey -r "^L"
+
+setopt incappendhistory
+
+eval "$(starship init zsh)"
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
---
+++ zsh/aliases.zsh
@@ -0,0 +1,171 @@
+#!/bin/sh
+alias j='z'
+alias f='zi'
+alias g='lazygit'
+alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
+alias nvimrc='nvim ~/.config/nvim/'
+alias yay="paru"
+
+# alias lvim='nvim -u ~/.local/share/lunarvim/lvim/init.lua --cmd "set runtimepath+=~/.local/share/lunarvim/lvim"'
+
+# get fastest mirrors
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+
+# Remarkable
+alias remarkable_ssh='ssh root@10.11.99.1'
+alias restream='restream -p'
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# easier to read disk
+alias df='df -h'     # human-readable sizes
+alias free='free -m' # show sizes in MB
+
+# get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4 | head -5'
+
+# get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3 | head -5'
+
+# gpg encryption
+# verify signature for isos
+alias gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
+# receive the key of a developer
+alias gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
+
+# For when keys break
+alias archlinx-fix-keys="sudo pacman-key --init && sudo pacman-key --populate archlinux && sudo pacman-key --refresh-keys"
+
+# systemd
+alias mach_list_systemctl="systemctl list-unit-files --state=enabled"
+
+alias mach_java_mode="export SDKMAN_DIR="$HOME/.sdkman" && [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh""
+
+alias m="git checkout master"
+alias s="git checkout stable"
+
+if [[ $TERM == "xterm-kitty" ]]; then
+  alias ssh="kitty +kitten ssh"
+fi
+
+case "$(uname -s)" in
+
+Darwin)
+	# echo 'Mac OS X'
+	alias ls='ls -G'
+	;;
+
+Linux)
+	alias ls='ls --color=auto'
+	;;
+
+CYGWIN* | MINGW32* | MSYS* | MINGW*)
+	# echo 'MS Windows'
+	;;
+*)
+	# echo 'Other OS'
+	;;
+esac
+
+
+alias rp='realpath'
+# disable cursor blink
+#echo "\e[2 q"
+alias vim=nvim
+alias v=nvim
+alias vi=nvim
+alias lg='lazygit'
+alias zj='zellij'
+alias tms='bash /home/crhf/dotfiles/tmux-sessionizer.sh'
+
+tmux_sessionize() {
+  if [[ $# -eq 1 ]]; then
+      selected=$1
+  else
+      selected=$(find ~/persistent ~/projects $(echo ~/projects/*) ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
+  fi
+
+  if [[ -z $selected ]]; then
+    return
+  fi
+
+  selected_name=$(basename "$selected" | tr . _)
+  tmux_running=$(pgrep tmux)
+
+  if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+      tmux new-session -s $selected_name -c $selected
+      return 0
+  fi
+
+  if ! tmux has-session -t=$selected_name 2> /dev/null; then
+      tmux new-session -ds $selected_name -c $selected
+  fi
+
+  if [[ -z $TMUX ]]; then
+    tmux attach -t $selected_name
+  else
+    tmux switch-client -t $selected_name
+  fi
+
+  return 0
+}
+zle -N tmux_sessionize
+# bindkey ^t tmux_sessionize
+
+ssh_with_password() {
+  if [ $# -ne 1 ]; then
+    ssh "$@"
+  else
+    case "$1" in
+      fat)
+        sshpass -p $(cat ~/.ssh/password.txt) ssh "$@"
+        ;;
+      *)
+        ssh "$@"
+        ;;
+    esac
+  fi
+}
+alias ssh='ssh_with_password'
+
+alias gl='git --no-pager log --oneline -5'
+alias gs='git status'
+alias gb='git branch'
+alias gc='git commit -m'
+alias ga='git add'
+alias gw='git show'
+alias gd='git diff'
+
+alias mkvv='mkvirtualenv'
+alias lsvv='lsvirtualenv'
+alias showvv='showvirtualenv'
+alias rmvv='rmvirtualenv'
+alias cpvv='cpvirtualenv'
+alias allvv='allvirtualenv'
+alias wo='workon'
+alias da='deactivate'
+
+alias vim='nvim'
+alias v='nvim'
+
+alias s='source'
+
+alias ls='ls -F --color=auto'
+alias l='ls'
+alias la='ls -a'
+alias ll='ls -l'
+
+alias c='clear'
+
+alias us='underscore'
