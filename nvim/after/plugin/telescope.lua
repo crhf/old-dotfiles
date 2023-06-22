9d4a97fd4d6f86045372d588bb95d5ea6bbccb85
crhf <ruanhf8@gmail.com>
2023-06-22 12:29:01 +0800

    feat: change telescope settings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,10 +3,10 @@ local mappings = {
         ["<C-l>"] = "close",
         ["<Esc>"] = false,
         ["<C-c>"] = false,
-        ["<C-j>"] = "move_selection_next",
-        ["<C-k>"] = "move_selection_previous",
-        ["<C-p>"] = false,
-        ["<C-n>"] = false,
+        -- ["<C-j>"] = "move_selection_next",
+        -- ["<C-k>"] = "move_selection_previous",
+        -- ["<C-p>"] = false,
+        -- ["<C-n>"] = false,
     },
 }
 require("telescope").setup {
@@ -38,6 +38,9 @@ require("telescope").setup {
         oldfiles = {
             mappings = mappings,
         },
+        diagnostics = {
+            mappings = mappings,
+        },
     },
 }
 local builtin = require('telescope.builtin')
@@ -58,3 +61,4 @@ vim.keymap.set('n', '<leader>/', function()
 end, { desc = '[/] Fuzzily search in current buffer' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
