068a1ba14a21b62b8aafe48ec8133d33dc586288
crhf <ruanhf8@gmail.com>
2023-06-22 18:58:08 +0800

    feat: turn on diagnostic icons in nvim-tree

---
+++ nvim/after/plugin/nvim_tree.lua
@@ -0,0 +1,18 @@
+require("nvim-tree").setup {
+    diagnostics = {
+        enable = true,
+        show_on_dirs = false,
+        show_on_open_dirs = true,
+        debounce_delay = 50,
+        severity = {
+            min = vim.diagnostic.severity.HINT,
+            max = vim.diagnostic.severity.ERROR,
+        },
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
+}
