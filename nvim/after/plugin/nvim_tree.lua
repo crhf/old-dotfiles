f0188eeecd9ce6b73cca1d9433159cdd3a958d82
crhf <ruanhf8@gmail.com>
2023-07-12 20:32:13 +0800

    feat: change nvim-tree mapping

--- nvim/after/plugin/nvim_tree.lua
+++ nvim/after/plugin/nvim_tree.lua
@@ -16,3 +16,6 @@ require("nvim-tree").setup {
         },
     },
 }
+local api = require('nvim-tree.api')
+vim.keymap.set('n', '<leader>tt', function() api.tree.toggle({ focus = false }) end)
+vim.keymap.set("n", "<leader>pv", function() api.tree.focus() end)
--- nvim/after/plugin/tree.lua
+++
@@ -1,3 +0,0 @@
-local api = require('nvim-tree.api')
-vim.keymap.set('n', '<leader>t', function() api.tree.toggle({focus=false}) end)
-vim.keymap.set("n", "<leader>pv", function () api.tree.focus() end)
