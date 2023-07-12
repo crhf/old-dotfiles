bf3bc48f77dda8586b4448c04d672f0377e2f45e
crhf <ruanhf8@gmail.com>
2023-07-12 20:35:46 +0800

    feat: add which-key

--- nvim/lua/crhf/packer.lua
+++ nvim/lua/crhf/packer.lua
@@ -157,6 +157,20 @@ return require('packer').startup(function(use)
     use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
     use 'theHamsta/nvim-dap-virtual-text'
     use 'JRasmusBm/telescope-dap.nvim'
+
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
     use {
         "nvim-neotest/neotest",
         requires = {
