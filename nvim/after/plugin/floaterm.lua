81870fe3b0da24d82a3e578eab5f372f8dbabae6
crhf <ruanhf8@gmail.com>
2023-08-08 12:04:21 +0800

    feat: configure hop.nvim

--- nvim/after/plugin/floaterm.lua
+++ nvim/after/plugin/floaterm.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<C-L>", ":FloatermToggle<CR>")
 vim.keymap.set("t", "<C-L>", "<C-\\><C-n>:FloatermToggle<CR>")
-vim.keymap.set("n", "<leader>ll", ":FloatermNew --name=git --title=git --disposable lazygit<CR>")
+vim.keymap.set("n", "<leader>lg", ":FloatermNew --name=git --title=git --disposable lazygit<CR>")
