d7e2bde7d41286e942ebf4e7257693c8cec52517
crhf <ruanhf8@gmail.com>
2023-08-10 13:38:09 +0800

    feat: change dap mappings

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -1,20 +1,21 @@
-vim.keymap.set("n", "<leader>1", require('dap').restart)
-vim.keymap.set("n", "<leader>2", require('dap').terminate)
-vim.keymap.set("n", "<leader>5", require('dap').continue)
-vim.keymap.set("n", "<leader>7", require('dap').step_into)
-vim.keymap.set("n", "<leader>8", require('dap').step_over)
-vim.keymap.set("n", "<leader>9", require('dap').run_to_cursor)
-vim.keymap.set("n", "<leader>0", require('dap').step_out)
+vim.keymap.set("n", "<leader>1", require('dap').restart, {desc="dap restart"})
+vim.keymap.set("n", "<leader>2", require('dap').terminate, {desc="dap terminate"})
+vim.keymap.set("n", "<leader>3", require('dap').restart_frame, {desc="dap restart frame"})
+vim.keymap.set("n", "<leader>5", require('dap').continue, {desc="dap continue"})
+vim.keymap.set("n", "<leader>7", require('dap').step_into, {desc="dap step into"})
+vim.keymap.set("n", "<leader>8", require('dap').step_over, {desc="dap step over"})
+vim.keymap.set("n", "<leader>9", require('dap').step_out, {desc="dap step out"})
+vim.keymap.set("n", "<leader>0", require('dap').run_to_cursor, {desc="dap run to cursor"})
 
-vim.keymap.set("n", "<leader>fk", require('dap').up)
-vim.keymap.set("n", "<leader>fj", require('dap').down)
+vim.keymap.set("n", "<leader>dk", require('dap').up, {desc="dap up"})
+vim.keymap.set("n", "<leader>dj", require('dap').down, {desc="dap down"})
 
-vim.keymap.set("n", "<leader>b", require('dap').toggle_breakpoint)
-vim.keymap.set("n", "<leader>B", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+vim.keymap.set("n", "<leader>db", require('dap').toggle_breakpoint, {desc="dap breakpoint"})
+vim.keymap.set("n", "<leader>dB", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", {desc="dap conditional breakpoint"})
 
-vim.keymap.set('n', '<Leader>lp',
-    function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<Leader>dr', require('dap').repl.open)
-vim.keymap.set('n', '<Leader>dl', require('dap').run_last)
+vim.keymap.set('n', '<Leader>dp',
+    function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end, {desc="dap log point"})
+vim.keymap.set('n', '<Leader>dr', require('dap').repl.open, {desc="dap repl"})
+vim.keymap.set('n', '<Leader>dl', require('dap').run_last, {desc="dap run last"})
 
 -- require('dap').defaults.fallback.terminal_win_cmd = 'terminal'
--- nvim/after/plugin/dapui.lua
+++ nvim/after/plugin/dapui.lua
@@ -1,2 +1,15 @@
 require("dapui").setup()
 vim.keymap.set('n', "<leader>ui", require('dapui').toggle)
+vim.keymap.set("n", "<leader>4", function() require('dapui').float_element(nil, {enter=true}) end, {desc="dapui float"})
+vim.keymap.set("n", "<leader>de", function() require('dapui').eval() end, {desc="dapui eval"})
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
