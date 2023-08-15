c068e42a089007ed51533f63207959c50d620d27
crhf <ruanhf8@gmail.com>
2023-08-15 13:09:07 +0800

    feat: tweak hardtime

--- nvim/after/plugin/hardtime.lua
+++ nvim/after/plugin/hardtime.lua
@@ -4,5 +4,8 @@ require("hardtime").setup({
         ["<DOWN>"] = {},
         ["<LEFT>"] = {},
         ["<RIGHT>"] = {},
-    }
+    },
+    max_count = 3,
+    allow_different_key = true,
+    disable_mouse = false
 })
