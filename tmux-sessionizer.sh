8db91a4be454a28faaeec9aa558ef9d961053cf1
crhf <ruanhf8@gmail.com>
2023-08-21 20:49:09 +0800

    feat: config tmux sessionizer

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -1,28 +1,61 @@
 #!/usr/bin/env bash
+# I organize my projects in ~/Developer/org/name, and I'd like to create sessions for each of them.
+# This script uses a combination of find, zoxide (so they are sort by frecency) and fzf to list and filter project folders.
+# Then, it either switch to an existing tmux session for that project, or create a new one and switch to it.
+#
+# Stole and modified from: https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/bin/tmux-sessionizer
 
-if [[ $# -eq 1 ]]; then
-    selected=$1
+PROJECTS="$HOME/projects $(echo ~/projects/*) $HOME"
+
+# increase frecency
+increase() {
+	selected="$1"
+	if [ "$selected" = default ]; then
+		return 0
+	fi
+	# zoxide add "$PROJECTS/$selected"
+	zoxide add "$selected"
+}
+
+search() {
+	find $PROJECTS -maxdepth 1 -type d |
+		while read -r p; do
+			zoxide query -l -s "$p/"
+		done |
+		sort -rnk1 |       # sort by score
+		uniq |             # dedup
+		awk '{print $2}' | # do not actually print the score
+		fzf --no-sort --prompt "  "
+}
+
+if [ "$#" -eq 1 ]; then
+	selected="$1"
 else
-    selected=$(find ~/persistent ~/projects ~/projects/* ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
+	selected=$(search)
 fi
 
-if [[ -z $selected ]]; then
-    exit 0
+if test -z $selected; then
+	exit 0
 fi
 
-selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
+selected_name="$(echo "$selected" | tr . _)"
+tmux_running="$(pgrep tmux)"
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
+if test -z "$TMUX" && test -z "$tmux_running"; then
+	# tmux new-session -s "$selected_name" -c "$PROJECTS/$selected"
+	tmux new-session -s "$selected_name" -c "$selected"
+	increase "$selected"
+	exit 0
+fi
+
+if ! tmux has-session -t="$selected_name" 2>/dev/null; then
+	# tmux new-session -ds "$selected_name" -c "$PROJECTS/$selected"
+	tmux new-session -ds "$selected_name" -c "$selected"
+fi
+
+if test -z "$TMUX"; then
+	tmux attach -t "$selected_name"
 else
-    if ! tmux has-session -t=$selected_name 2> /dev/null; then
-        tmux new-session -ds $selected_name -c $selected
-    fi
-
-    if [[ -z $TMUX ]]; then
-        tmux attach -t $selected_name
-    else
-        tmux switch-client -t $selected_name
-    fi
+	tmux switch-client -t "$selected_name"
 fi
+increase "$selected"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -87,41 +87,13 @@ alias v=nvim
 alias vi=nvim
 alias lg='lazygit'
 alias zj='zellij'
-alias tms='bash /home/crhf/dotfiles/tmux-sessionizer.sh'
 
-tmux_sessionize() {
-  if [[ $# -eq 1 ]]; then
-      selected=$1
-  else
-      selected=$(find ~/persistent ~/projects $(echo ~/projects/*) ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
-  fi
-
-  if [[ -z $selected ]]; then
-    return
-  fi
-
-  selected_name=$(basename "$selected" | tr . _)
-  tmux_running=$(pgrep tmux)
-
-  if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-      tmux new-session -s $selected_name -c $selected
-      return 0
-  fi
-
-  if ! tmux has-session -t=$selected_name 2> /dev/null; then
-      tmux new-session -ds $selected_name -c $selected
-  fi
-
-  if [[ -z $TMUX ]]; then
-    tmux attach -t $selected_name
-  else
-    tmux switch-client -t $selected_name
-  fi
-
-  return 0
+function tmux_sessionize {
+  BUFFER="/home/crhf/dotfiles/tmux-sessionizer.sh"
+  zle accept-line
 }
-zle -N tmux_sessionize
-# bindkey ^t tmux_sessionize
+zle -N tmux_sessionize tmux_sessionize
+bindkey '^t' "tmux_sessionize"
 
 ssh_with_password() {
   if [ $# -ne 1 ]; then
