4760c1a906fd78521232373ae9dcd73c75c62644
crhf <ruanhf8@gmail.com>
2023-08-25 20:33:23 +0800

    feat: configure snippets

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -21,7 +21,7 @@ lsp.on_attach(function(client, bufnr)
     -- rc.signature_help = false
     -- rc.format = false
     -- rc.completion = false
-        -- rc.references = false
+    -- rc.references = false
     -- end
 
     lsp.default_keymaps({ buffer = bufnr })
@@ -46,19 +46,44 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
-require('lspconfig').pyright.setup({
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                diagnosticMode = "workspace",
-                useLibraryCodeForTypes = true,
-                autoImportCompletions = false,
-            },
-        },
-        linting = { pylintEnabled = false }
-    },
-})
+-- require('lspconfig').pyright.setup({
+--     settings = {
+--         python = {
+--             analysis = {
+--                 autoSearchPaths = true,
+--                 diagnosticMode = "workspace",
+--                 useLibraryCodeForTypes = true,
+--                 autoImportCompletions = false,
+--             },
+--         },
+--         linting = { pylintEnabled = false }
+--     },
+-- })
+
+-- require('lspconfig').pylsp.setup({
+--     settings = {
+--         pylsp = {
+--             plugins = {
+--                 rope_autoimport = {
+--                     enabled = true
+--                 },
+--                 autopep8 = {
+--                     enabled = false
+--                 },
+--                 pycodestyle = {
+--                     enabled = false
+--                 },
+--                 jedi_completion = {
+--                     enabled = false
+--                 },
+--                 pyflakes = {
+--                     enabled = false
+--                 }
+--             }
+--
+--         }
+--     }
+-- })
 
 -- require('lspconfig').pyright.setup({
 --     on_init = function(client)
@@ -67,23 +92,90 @@ require('lspconfig').pyright.setup({
 -- })
 -- lsp.skip_server_setup({'pyright'})
 
+lsp.setup() -- after specific lang servers, before cmp
+
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local luasnip = require("luasnip")
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    -- ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            -- cmp.select_next_item()
+            cmp.confirm({ select = true })
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- they way you will only jump inside the snippet region
+        elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+        elseif has_words_before() then
+            cmp.complete()
+        else
+            fallback()
+        end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+        else
+            fallback()
+        end
+    end, { "i", "s" }),
     -- ["<C-Space>"] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
 })
 
-lsp.setup_nvim_cmp({
+cmp.setup({
     mapping = cmp_mappings,
     window = {
         documentation = cmp.config.window.bordered()
-    }
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        -- { name = 'vsnip' }, -- For vsnip users.
+        { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = 'buffer' },
+    }),
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+    -- performance = {
+    --     debounce = 10,
+    --     throttle = 0,
+    --     fetching_timeout = 10
+    -- }
 })
 
-lsp.setup()
+-- lsp.setup_nvim_cmp({
+--     mapping = cmp_mappings,
+--     window = {
+--         documentation = cmp.config.window.bordered()
+--     },
+--     -- performance = {
+--     --     debounce = 10,
+--     --     throttle = 0,
+--     --     fetching_timeout = 10
+--     -- }
+-- })
+
 
 --[[
 lsp.on_attach(function(client, bufnr)
